f759cf5e0b6c900bb1c44e796159e5c8f4349fd1
Tim Kopp <tim.kopp@cyber-balance.com>
2020-09-09 19:00:22 -0400

    added file io

---
+++ fileio.py
@@ -0,0 +1,16 @@
+file = open("data\\phonetic_alphabet.txt")
+file_text = file.read()
+file.close()
+
+new_text = ''
+
+lines = file_text.split('\n')
+for line in lines:
+    #print(line)        #for debug
+    new_text += '' + line + '\n' 
+    #print(new_text)    #for debug
+
+print(new_text)        #for debug
+#newfile = open("newfile.txt", "w")
+#newfile.write(new_text)
+#newfile.close()
