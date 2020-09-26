98aa9cf3eaf373831a6c1ea6d8177b6c3c850813
Tim Kopp <tim.kopp@cyber-balance.com>
2020-09-26 12:23:26 -0400

    added dataframe

---
+++ dataframe.py
@@ -0,0 +1,15 @@
+import pandas as pd
+data = {
+'country': ['France', 'Spain', 'Italy'],
+'city':['Paris', 'Madrid', 'Rome'], 
+'days': [10, 12, 7]
+}
+
+row_labels = [1, 2, 3]
+df = pd.DataFrame(data=data, index=row_labels)
+#print(df)
+
+for i, j in df.iterrows():
+    print(i, j )
+  
+ 
