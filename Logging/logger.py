1317a9e643f7824e12546f9bfff9c353c2dbf4b3
Tim Kopp <timkopp@gmail.com>
2021-10-02 11:24:20 -0400

    refactor elapsed logger 11371

--- Logging/logger.py
+++ Logging/logger.py
@@ -20,14 +20,14 @@ class Logger:
         d['level'] = str(LogLevel(loglevel))
         d['severity'] = loglevel.value
         d['elapsed'] = self.__log_time_elapse()
-        d['time'] = self.__log_time()
+        d['runtime'] = self.__log_time()
         d['source'] = src
         d['message'] = msg
         return d
     def __log_time_elapse(self):
-        return str(round(time.time()-self.time_init, 2))
+        return str(round(time.time()-self.time_init, 4))
     def __log_time(self):
-        t = str(round(time.time()-self.time_last, 2))
+        t = str(round(time.time()-self.time_last, 4))
         self.time_last = time.time()
         return t 
     def LogInfo(self, source, message): 
@@ -36,7 +36,7 @@ class Logger:
         d = self.__get_log_item(LogLevel.Debug, source, message)
         self.__log.append(d) 
         if self.Verbose: 
-            print(f" ({d['time']}) {d['source']}: {d['message']} ")
+            print(f" ({d['runtime']}) {d['source']}: {d['message']} ")
     def LogWarn(self, source, message):
         self.__log.append(self.__get_log_item(LogLevel.Warn, source, message)) 
     def LogException(self, source, message):
@@ -46,7 +46,7 @@ class Logger:
         return logs  
     def show(self): 
         for d in self.get_log():
-            print(f" {LogLevel(d['severity'])}: {d['source']} {d['message']} ({d['time']}) " )
+            print(f" {LogLevel(d['severity'])}: {d['source']} {d['message']} ({d['runtime']}) " )
     def to_json(self, path):  
         with open(f'{path}', "w") as f:
             json.dump(self.get_log(), f, indent=4 )
