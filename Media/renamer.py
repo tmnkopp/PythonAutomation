c89be2e7282d92ccdcc26c2246b5b2329620631e
Tim Kopp <timkopp@gmail.com>
2022-02-14 15:21:06 -0500

    refactor gen files 109104

--- Media/renamer.py
+++
@@ -1,9 +0,0 @@
-import os, re
- 
-for d, dirs, files in os.walk(r"E:\007\Skyfall"): 
-    for fi in files:  
-        prev = os.path.join(d, fi)
-        nam = re.sub(r'cd(\d)',r'\1',fi)
-        next = os.path.join(d, nam)
-        os.rename(prev, next)
-        print(  os.path.join(d, nam)  )   
