bdd6133213d49754a97c80f48463f33b77cbb5fb
Tim Kopp <timkopp@gmail.com>
2022-11-16 20:04:48 -0500

    Compile codegen fixes #112118

--- CodeGenerators/lib/questionnaire_parser.py
+++ CodeGenerators/lib/questionnaire_parser.py
@@ -9,15 +9,19 @@ class questionnaire_parser():
         converts={'Selections':lambda x: re.sub('\d{,2}\.','',x)}
         df=pd.read_excel(self.ctx.config['source'], converters=converts, sheet_name=int(self.ctx.config['sheet']), header=30, usecols='A,B,D').fillna(method='ffill').reset_index()
         df=df.groupby(['ID','Question'], as_index=False).agg({'Selections': list, 'index': 'min'})
-        df=df.sort_values('index').reset_index(drop=True).drop('index', axis=1)
-        df['DATATYPE']=df['Selections'].apply(self._get_datatype) 
+        df=df.sort_values('index').reset_index(drop=True).drop('index', axis=1) 
+        df['DATATYPE']=df['ID']
+        df['DATATYPE']=[self._get_datatype(s) for s in zip(df['ID'], df['Selections'])] 
         pr = picklist_recommender(self.ctx.config['connstr']) 
         df['PLT']=df['Selections'].apply(lambda x: pr.recommend(x)['PK_PicklistType'] ) 
         pr.to_cache()
 
         return df
-    def _get_datatype(self, s):
-        if re.search('Integer',''.join(s)) : return 'INT'
-        if  len(s) < 2  : return 'TXT'
-        return 'PICK'
  
+    def _get_datatype(self, t): 
+        id,sel=t
+        if re.search('Integer',''.join(sel)) : return 'INT' 
+        if len(sel) < 2  : return 'TXT'
+        if re.search(self.ctx.config['chkbox_pattern'],''.join(id)) : return 'MULTICHECKBOX'
+        return 'PICK'        
+     
