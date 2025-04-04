f8426f2da991016e5bd5dd732ce622be64e3d123
Tim Kopp <timkopp@gmail.com>
2025-04-04 10:49:46 -0400

    refactor jra provider : 9972

--- DeStruct/solve.py
+++ DeStruct/solve.py
@@ -1,25 +1,13 @@
  
-nums = [-1,0,1,2,-1,-4]
-nums = sorted(nums)
-result = []
-sub = []
-def recur(sub, index, tnums): 
-    if len(sub) == 3:
-        if sum(sub) == 0:
-            new_list = sorted([i for i in sub])
-            if new_list not in result:
-                result.append(new_list)
-        return
-    for i in tnums[:]:
-        sub.append(i)
-        tnums = tnums[1:]
-        recur(sub, index+1, tnums)
-        sub.pop()
-    return result
-recur([], 0, nums)
-print(result)
+nums = [5,7,7,8,8,10]
+target = 8
+dp=(-1,-1)
+for i in range(len(nums)):
+    if nums[i]==target:
+        if dp[0] < 0 :
+           dp = (i,-1)
+        elif dp[0] >= 0: 
+            dp = (dp[0], i)
 
-
-
-
+print(dp)
 
